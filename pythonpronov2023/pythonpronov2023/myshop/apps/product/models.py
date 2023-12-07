11c8aaf4064569bc5edf7237f28acc27bcfb95aa
SergeyKund <143610718+SergeyKund@users.noreply.github.com>
2023-12-07 14:17:33 +0200

    Add files via upload

--- pythonpronov2023/pythonpronov2023/myshop/apps/product/models.py
+++ pythonpronov2023/pythonpronov2023/myshop/apps/product/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from apps.brand.models import Brand
 
 
 
@@ -9,9 +9,12 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=12, decimal_places=2)
     created_at = models.DateTimeField(auto_created=True)
     description = models.TextField(null=True, blank=True)
-
+    brand = models.ForeignKey(Brand, on_delete=models.CASCADE, null=True, blank=True)
 
 
 
     def __str__(self):
         return f'{self.name}: {self.price}'
+
+    class Meta:
+        ordering = ['name']
